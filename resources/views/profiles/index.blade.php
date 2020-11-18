50810a1c43261bf1051407a8eac4c8b8a47a08fe
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-11-18 08:26:19 +0600

    Profile page updated

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -35,7 +35,7 @@
 
     <div class="row pt-4 pb-4">
         @foreach($user->posts as $post)
-        <div class="col-4">
+        <div class="col-4 mb-4">
             <a href="/p/{{ $post-> id}}">
                 <img src="/storage/{{ $post->image }}" class="w-100">  
             </a>
